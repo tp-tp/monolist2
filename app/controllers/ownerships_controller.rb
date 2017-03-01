1ded44b94ea61f25b58264049a98d57313089b4d
toshihirooda <o.d.a.t.o.p.p.y@gmail.com>
2017-03-01 01:07:55 +0000

    implement have and want button

--- app/controllers/ownerships_controller.rb
+++ app/controllers/ownerships_controller.rb
@@ -10,8 +10,8 @@ class OwnershipsController < ApplicationController
 
     # itemsテーブルに存在しない場合は楽天のデータを登録する。
     if @item.new_record?
-      # TODO 商品情報の取得 RakutenWebService::Ichiba::Item.search を用いてください
-      items = {}
+      # TODO 商品情報の取得 RakuteniWebService::Ichiba::Item.search を用いてください
+      items = RakutenWebService::Ichiba::Item.search(itemCode: params[:item_code])
 
       item                  = items.first
       @item.title           = item['itemName']
@@ -26,7 +26,11 @@ class OwnershipsController < ApplicationController
     # params[:type]の値にHaveボタンが押された時には「Have」,
     # Wantボタンが押された時には「Want」が設定されています。
     
-
+    if params[:type] ==  "Have"
+      current_user.have(@item)
+    elsif params[:type] ==  "Want"
+      current_user.want(@item)
+    end
   end
 
   def destroy
@@ -35,6 +39,10 @@ class OwnershipsController < ApplicationController
     # TODO 紐付けの解除。 
     # params[:type]の値にHave itボタンが押された時には「Have」,
     # Want itボタンが押された時には「Want」が設定されています。
-
+    if params[:type] ==  "Have"
+      current_user.unhave(@item)
+    elsif params[:type] ==  "Want"
+      current_user.unwant(@item)
+    end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,12 @@ class User < ActiveRecord::Base
 
   has_many :ownerships , foreign_key: "user_id", dependent: :destroy
   has_many :items ,through: :ownerships
+  
+  has_many :wants, class_name: "Want", foreign_key: "user_id", dependent: :destroy
+  has_many :want_items , through: :wants, source: :item
 
+  has_many :haves, class_name: "Have", foreign_key: "user_id", dependent: :destroy
+  has_many :have_items , through: :haves, source: :item
 
   # 他のユーザーをフォローする
   def follow(other_user)
@@ -31,20 +36,32 @@ class User < ActiveRecord::Base
 
   ## TODO 実装
   def have(item)
+    haves.find_or_create_by(item_id: item.id)
   end
 
   def unhave(item)
+    #have_items.find_by(haved_id: other_user.id).destroy
+    have = haves.find_by(item_id: item.id)
+    have.destroy if have
   end
 
   def have?(item)
+    have_items.include?(item)
   end
 
   def want(item)
+    #want_items.create(wanted_id: other_user.id)
+    wants.find_or_create_by(item_id: item.id)
   end
 
   def unwant(item)
+    #want_items.find_by(wanted_id: other_user.id).destroy
+    want = wants.find_by(item_id: item_id)
+    want.destroy if want
   end
 
   def want?(item)
+    #wants.include?(other_user)
+    want_items.include?(item)
   end
 end
